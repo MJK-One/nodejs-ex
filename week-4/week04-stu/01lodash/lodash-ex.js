2d1ad954bfe6ed80942b1309f66d30f623c3f1c9
김민준 <hanleejss012@naver.com>
2024-03-26 21:30:52 +0900

    week-4

---
+++ week-4/week04-stu/01lodash/lodash-ex.js
@@ -0,0 +1,2 @@
+// ./01lodash/lodash-ex.js 외부 모듈 실행
+var _ = require('lodash');
---
+++ week-4/week04-stu/02http/http-ex.js
@@ -0,0 +1,8 @@
+const http = require("node:http");
+const server = http.createServer( (req, res) => {
+    console.log("요청 발생");
+});
+
+server.listen( 3000, () => {
+    console.log('3000번 포트에서 서버 실행중');
+});
